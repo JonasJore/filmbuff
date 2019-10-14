29037ac2cf78dde26a4f71ad0c75537bf7b2f2d2
JonasJore <jonas.jore@gmail.com>
2019-10-14 21:43:07 +0200

    initial commit :coffee:

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # filmbuff
-frontend project for fans of movies
+
+frontend project for fans of movies 📽🍿
+
+## Building and running on localhost
+
+First install dependencies:
+
+```sh
+npm install
+```
+
+To create a production build:
+
+```sh
+npm run build-prod
+```
+
+To create a development build:
+
+```sh
+npm run build-dev
+```
+
+## Running
+
+Open the file `dist/index.html` in your browser
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    port: 6969,
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ]
+  }
+};
+
+module.exports = config;
